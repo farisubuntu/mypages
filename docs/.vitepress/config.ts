bc35a0f47c5f4cb2aceadd44e14925c981435123
farisubuntu <farisubuntu@gmail.com>
2023-12-18 07:32:44 +0300

    fix

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       //   ],
       // },
 
-      // ...
+      //...
     ],
 
     sidebar: [
@@ -32,4 +32,7 @@ export default defineConfig({
       },
     ],
   },
+base: '/mypages/',
+srcDir: './src'
+
 });
