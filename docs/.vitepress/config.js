c02ba0b8f0e93c75aa1f77716bd16b47e48c16fd
farisubuntu <farisubuntu@gmail.com>
2023-12-19 09:13:21 +0300

    startup

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+ title:'My Pages', //append to all page titles
+ themeConfig:{
+   nav: [
+    { text: 'Home', link : '/'},
+    { text: 'About', link: '/about/'},
+    {text: 'Contact', link: '/contact'},
+    {text: 'PDFs', link: '/pdfs/'}
+   ],
+   sidebar: [
+    {text: 'CSS',link: '/css/'},
+   {text: 'Javascript', link:'/javascript/'},
+   {text:'Linux',link: '/Linux/'},
+   {text:'Git and Github',link:'/Github/'},
+   {text:'Network',link:'/Network_Net'},
+   {text: 'PHP',link: '/PHP/'},
+   ],
+ },
+}
