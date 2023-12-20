ff1a628e2a717e7bdcbb2ce41903f9abe5a120d6
farisubuntu <farisubuntu@gmail.com>
2023-12-20 05:58:12 +0300

    comment styles

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,8 +1,8 @@
 import DefaultTheme from "vitepress/theme";
 import "./custom.css";
-import "./header.css";
-import "./navbar.css";
-import "./sidebar.css";
+// import "./header.css";
+// import "./navbar.css";
+// import "./sidebar.css";
 
 
 export default DefaultTheme;
